5997c1f4648ee673edb959c1061ba119f0bd7c8c
soc-macmini-31 <soc-macmini-31@admins-Mac-mini.local>
2019-02-26 10:19:03 +0530

    changing background colour of label

--- gitPractice/ViewController.swift
+++ gitPractice/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        helloLabel.backgroundColor = UIColor.red
     }
 
 
